972e984c606bae10a4fcc23dd905b39ba387f46e
Michal Borowski <unpolacoo@gmail.com>
2020-11-04 23:17:54 +0100

    Create Global reset styles

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {App} from './components/App';
+import {reset} from './resetStyles';
+import {css, Global} from '@emotion/core';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <>
+        <Global
+            styles={css`
+                ${reset}
+            `}
+        />
+        <App />
+    </>,
+    document.getElementById('root'),
+);
