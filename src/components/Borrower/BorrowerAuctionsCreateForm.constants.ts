4f469832e4972406e55a727ffb1aa1dd37b56387
Michal Borowski <unpolacoo@gmail.com>
2020-11-05 23:44:08 +0100

    Creates BorrowerAuctionsCreateForm types and constants

---
+++ src/components/Borrower/BorrowerAuctionsCreateForm.constants.ts
@@ -0,0 +1,7 @@
+import {AuctionCreateFormValues} from './BorrowerAuctionsCreateForm.types';
+
+export const initialValues: AuctionCreateFormValues = {amount: '', rate: '', loanDuration: 0, startDate: ''};
+export const marks = [
+    {value: 0, label: '0'},
+    {value: 36, label: '36'},
+];
