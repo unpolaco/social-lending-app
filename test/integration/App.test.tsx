5de5fe0662b4f009ca28c12dce79cde8adc1a472
Michal Borowski <unpolacoo@gmail.com>
2020-11-06 13:38:57 +0100

    Correct path in App.test.tsx

--- test/integration/App.test.tsx
+++ test/integration/App.test.tsx
@@ -1,5 +1,5 @@
 import {render, screen} from '@testing-library/react';
-import {App} from '../../src/components/App';
+import {App} from '../../src/components/App/App';
 import React from 'react';
 
 describe('<App/>', () => {
