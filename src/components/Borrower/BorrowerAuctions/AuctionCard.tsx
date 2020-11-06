151c32c2053e17da6dd2828a70d9219f7192fcde
Michal Borowski <unpolacoo@gmail.com>
2020-11-06 23:58:36 +0100

    Display fetched list of auctions

--- src/components/Borrower/BorrowerAuctions/AuctionCard.tsx
+++ src/components/Borrower/BorrowerAuctions/AuctionCard.tsx
@@ -2,29 +2,31 @@ import React from 'react';
 import {Avatar, Grid, Typography} from '@material-ui/core/';
 import {AvatarGroup} from '@material-ui/lab';
 import {BoxFlexRow, BoxNarrow, AuctionCardWrapper} from './AuctionCard.styles';
-export const AuctionCard = () => {
+export const AuctionCard: React.FC<any> = ({auction}) => {
+    const {amount, auctionDuration, auctionStartDate, borrower, borrowerRating, id, offers, rate} = auction;
+
     return (
         <AuctionCardWrapper>
             <Grid container alignItems="center" justify="space-between">
                 <BoxFlexRow>
                     <Avatar sizes="60">MB</Avatar>
-                    <Typography variant="h6">Michał Borowski</Typography>
+                    <Typography variant="h6">{borrower}</Typography>
                 </BoxFlexRow>
                 <BoxNarrow>
                     <Typography variant="body2">auction amount</Typography>
-                    <Typography variant="h4">500zł</Typography>
+                    <Typography variant="h4">{amount}zł</Typography>
                 </BoxNarrow>
                 <BoxNarrow>
                     <Typography variant="body2">rate</Typography>
-                    <Typography variant="h4">5%</Typography>
+                    <Typography variant="h4">{rate}%</Typography>
                 </BoxNarrow>
                 <BoxNarrow>
                     <Typography variant="body2">loan start</Typography>
-                    <Typography>12 01 2021</Typography>
+                    <Typography>{auctionStartDate}</Typography>
                 </BoxNarrow>
                 <BoxNarrow>
                     <Typography variant="body2">duration</Typography>
-                    <Typography>3 months</Typography>
+                    <Typography>{auctionDuration} months</Typography>
                 </BoxNarrow>
                 <BoxNarrow>
                     <Typography variant="body2">offers</Typography>
@@ -37,3 +39,25 @@ export const AuctionCard = () => {
         </AuctionCardWrapper>
     );
 };
+
+// {
+//     "amount": 0,
+//     "auctionDuration": 0,
+//     "auctionStartDate": "2020-11-06",
+//     "borrower": "string",
+//     "borrowerRating": 0,
+//     "id": 0,
+//     "loanDuration": 0,
+//     "loanStartDate": "2020-11-06",
+//     "offers": [
+//       {
+//         "amount": 0,
+//         "auctionId": 0,
+//         "lenderUserName": "string",
+//         "offerId": 0,
+//         "rate": 0
+//       }
+//     ],
+//     "rate": 0,
+//     "status": "string"
+//   }
--- src/components/Borrower/BorrowerAuctions/BorrowerAuctions.tsx
+++ src/components/Borrower/BorrowerAuctions/BorrowerAuctions.tsx
@@ -1,13 +1,29 @@
-import React from 'react';
-import {Container, Typography} from '@material-ui/core/';
+import React, {useEffect} from 'react';
+import {Container, Typography, CircularProgress} from '@material-ui/core/';
 import {AuctionCard} from './AuctionCard';
 import {BorrowerAuctionsCreateForm} from './BorrowerAuctionsCreateForm/BorrowerAuctionsCreateForm';
+import {useGetAllAuctions} from '../../../hooks/useGetAllAuctions';
 export const BorrowerAuctions = () => {
+    const {isFetching, isError, fetchAllAuctions, auctionsList} = useGetAllAuctions();
+
+    useEffect(() => {
+        fetchAllAuctions();
+    }, [fetchAllAuctions]);
+
+    if (isFetching) {
+        return <CircularProgress />;
+    }
+    if (isError) {
+        alert('Error');
+    }
+
     return (
         <Container>
             <Typography>List of all actual auctions:</Typography>
             <BorrowerAuctionsCreateForm />
-            <AuctionCard />
+            {auctionsList?.map((auction: any) => (
+                <AuctionCard key={auction.id} auction={auction} />
+            ))}
         </Container>
     );
 };
