bd5e9f49da24f36a2b4d8839ac6bb560463f82c8
Michal Borowski <unpolacoo@gmail.com>
2020-11-09 13:04:47 +0100

    Create fetchNewOffer request

--- src/components/shared/AuctionTable/AuctionTable.tsx
+++ src/components/shared/AuctionTable/AuctionTable.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {TextBold, StyledTableRow} from './AuctionTable.styles';
+import {TextBold, StyledTableRow, WideCell, NarrowCell, StyledBox, CollapsedCell} from './AuctionTable.styles';
 import {
     Table,
     TableBody,
@@ -22,7 +22,7 @@ import {CollapseBoxLender} from '../AuctionTableCollapseBox/CollapseBoxLender';
 import {CollapseBoxBorrowerAllAuctions} from '../AuctionTableCollapseBox/CollapseBoxBorrowerAllAuctions';
 import {CollapseBoxBorrowerUserAuctions} from '../AuctionTableCollapseBox/CollapseBoxBorrowerUserAuctions';
 
-export const AuctionTable: React.FC<any> = ({auctionsList, lender, borrowerAllAuctions, borrowerUserAuctions}) => {
+export const AuctionTable: React.FC<any> = ({auctionsList, lender, borrowerAllAuctions, borrowerUserAuctions, handleSaveNewOffer}) => {
     const rows = auctionsList;
     const rowsPerPage = 5;
     const [order, setOrder] = React.useState<Order>('asc');
@@ -36,14 +36,11 @@ export const AuctionTable: React.FC<any> = ({auctionsList, lender, borrowerAllAu
         setOrder(isAsc ? 'desc' : 'asc');
         setOrderBy(property);
     };
-
     const handleChangePage = (event: unknown, newPage: number) => setPage(newPage);
-
     const handleClickCollapsed = (id: number | string) => {
         if (clickedCollapsed === id) setClickedCollapsed(null);
         else setClickedCollapsed(id);
     };
-
     return (
         <Paper>
             <TableContainer>
@@ -55,33 +52,40 @@ export const AuctionTable: React.FC<any> = ({auctionsList, lender, borrowerAllAu
                             .map((row, index) => (
                                 <>
                                     <StyledTableRow role="checkbox" tabIndex={-1} key={row.id} onClick={() => handleClickCollapsed(row.id)}>
-                                        <TableCell>
+                                        <WideCell>
+                                            <Avatar sizes="60">MB</Avatar>
+                                            <StyledBox>
+                                                <TextBold>{row.borrower}</TextBold>
+                                                <Rating size="small" value={+row.borrowerRating} readOnly />
+                                            </StyledBox>
+                                        </WideCell>
+                                        <NarrowCell align="right">
+                                            <TextBold>{row.amount} zł</TextBold>
+                                        </NarrowCell>
+                                        <NarrowCell align="right">{row.rate}%</NarrowCell>
+                                        <NarrowCell align="right">{row.auctionDuration} months</NarrowCell>
+                                        <NarrowCell align="right">{row.auctionStartDate}</NarrowCell>
+                                        <NarrowCell>
                                             <IconButton aria-label="expand row" size="small">
                                                 {clickedCollapsed === row.id ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
                                             </IconButton>
-                                        </TableCell>
-                                        <TableCell>
-                                            <Avatar sizes="60">MB</Avatar>
-                                        </TableCell>
-                                        <TableCell scope="row">
-                                            <TextBold>{row.borrower}</TextBold>
-                                            <Rating size="small" value={+row.borrowerRating} readOnly />
-                                        </TableCell>
-                                        <TableCell align="right">
-                                            <TextBold>{row.amount} zł</TextBold>
-                                        </TableCell>
-                                        <TableCell align="right">{row.rate}%</TableCell>
-                                        <TableCell align="right">{row.auctionDuration} months</TableCell>
-                                        <TableCell align="right">{row.auctionStartDate}</TableCell>
+                                        </NarrowCell>
                                     </StyledTableRow>
                                     <TableRow>
-                                        <TableCell>
+                                        <CollapsedCell colSpan={6}>
                                             <Collapse in={clickedCollapsed === row.id} timeout="auto" unmountOnExit>
-                                                {lender && <CollapseBoxLender />}
+                                                {lender && (
+                                                    <CollapseBoxLender
+                                                        borrowerAmount={row.amount}
+                                                        borrowerRate={row.rate}
+                                                        handleSaveNewOffer={handleSaveNewOffer}
+                                                        auctionId={row.id}
+                                                    />
+                                                )}
                                                 {borrowerAllAuctions && <CollapseBoxBorrowerAllAuctions row={row} />}
                                                 {borrowerUserAuctions && <CollapseBoxBorrowerUserAuctions />}
                                             </Collapse>
-                                        </TableCell>
+                                        </CollapsedCell>
                                     </TableRow>
                                 </>
                             ))}
