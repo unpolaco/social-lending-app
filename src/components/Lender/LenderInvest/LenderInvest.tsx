7c2c0e6d2ef85e026d83c00d8525730f42916e0a
Michal Borowski <unpolacoo@gmail.com>
2020-11-10 13:47:35 +0100

    Merge branch master into develop-auctions

--- src/components/Lender/LenderInvest/LenderInvest.tsx
+++ src/components/Lender/LenderInvest/LenderInvest.tsx
@@ -1,26 +1,34 @@
 import React, {useEffect} from 'react';
 import {Typography, CircularProgress} from '@material-ui/core/';
 import {useGetAllAuctions} from '../../../hooks/useGetAllAuctions';
+import {useSaveNewOffer} from '../../../hooks/useSaveNewOffer';
 import {AuctionTable} from '../../shared/AuctionTable/AuctionTable';
 
 export const LenderInvest = () => {
     const {isFetchingGet, isErrorGet, fetchAllAuctions, auctionsList} = useGetAllAuctions();
+    const {isFetchingSave, isErrorSave, fetchNewOffer} = useSaveNewOffer();
 
     useEffect(() => {
         fetchAllAuctions();
     }, [fetchAllAuctions]);
 
-    if (isFetchingGet) {
+    if (isFetchingGet || isFetchingSave) {
         return <CircularProgress />;
     }
-    if (isErrorGet) {
+    if (isErrorGet || isErrorSave) {
         alert('Error');
+    }
+
+    function handleSaveNewOffer(newOfferData: any) {
+        newOfferData.lenderUserName = 'testLender';
+        fetchNewOffer(newOfferData);
+        fetchAllAuctions();
     }
 
     return (
         <>
             <Typography>List of all actual auctions:</Typography>
-            {auctionsList && <AuctionTable auctionsList={auctionsList} lender />}
+            {auctionsList && <AuctionTable auctionsList={auctionsList} lender handleSaveNewOffer={handleSaveNewOffer} />}
         </>
     );
 };
