df9d638d1c5e4da11d3ec59b0f02f6610d03fac3
Michal Borowski <unpolacoo@gmail.com>
2020-11-10 13:35:29 +0100

    Merge changes

--- src/components/shared/AuctionTableCollapseBox/CollapseBoxLender.tsx
+++ src/components/shared/AuctionTableCollapseBox/CollapseBoxLender.tsx
@@ -1,11 +1,49 @@
 import React from 'react';
-import {Typography, Box, Button} from '@material-ui/core/';
+import {StyledBox} from './CollapseBoxLender.styles';
+import {Formik, Form} from 'formik';
+import {Button, Typography, TextField, Slider, InputAdornment} from '@material-ui/core';
 
-export const CollapseBoxLender: React.FC = () => {
+export const CollapseBoxLender: React.FC<any> = ({borrowerAmount, borrowerRate, handleSaveNewOffer, auctionId}) => {
+    const marks = [
+        {value: 0, label: '0 zł'},
+        {value: borrowerAmount, label: `${borrowerAmount} zł`},
+    ];
+    function handleSubmit(values: any) {
+        values.auctionId = auctionId;
+        handleSaveNewOffer(values);
+    }
+
     return (
-        <Box>
+        <StyledBox>
             <Typography>Create an offer for this auction</Typography>
-            <Button variant="outlined">Create offer</Button>
-        </Box>
+            <Formik initialValues={{amount: borrowerAmount, rate: borrowerRate}} onSubmit={handleSubmit}>
+                {({handleSubmit, values, handleChange, handleBlur, errors, touched, setFieldValue}) => (
+                    <Form onSubmit={handleSubmit}>
+                        <TextField
+                            name="rate"
+                            type="number"
+                            label={'Enter expected rate *'}
+                            InputProps={{endAdornment: <InputAdornment position="end">%</InputAdornment>}}
+                            value={values.rate}
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                            error={Boolean(touched.rate && errors.rate)}
+                            helperText={touched.rate && errors.rate}
+                        />
+                        <Typography>Your amount</Typography>
+                        <Slider
+                            name="amount"
+                            valueLabelDisplay="auto"
+                            min={0}
+                            max={borrowerAmount}
+                            marks={marks}
+                            value={values.amount}
+                            onChange={(e, value) => setFieldValue('amount', value)}
+                        />
+                        <Button type="submit">Create offer</Button>
+                    </Form>
+                )}
+            </Formik>
+        </StyledBox>
     );
 };
