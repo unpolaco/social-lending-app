1f1a59d98b8d18418c55d348f498d5ee1544f277
Michal Borowski <unpolacoo@gmail.com>
2020-11-18 13:35:47 +0100

    Display loanAmount in AuctionCreateForm

--- src/components/shared/AuctionCreateForm/AuctionCreateForm.tsx
+++ src/components/shared/AuctionCreateForm/AuctionCreateForm.tsx
@@ -23,57 +23,63 @@ export const AuctionCreateForm: React.FC<any> = ({handleSaveNewAuction}) => {
                 <AccordionWrapper>
                     <CreateAuctionCardWrapper>
                         <Formik initialValues={initialValues} validate={AuctionCreateFormValidator} onSubmit={handleSubmit}>
-                            {({handleSubmit, values, handleChange, handleBlur, errors, touched, setFieldValue, isValid}) => (
-                                <Form onSubmit={handleSubmit}>
-                                    <FormWrapper>
-                                        <TextField
-                                            autoFocus
-                                            name="amount"
-                                            type="number"
-                                            label={'Enter amount *'}
-                                            value={values.amount}
-                                            onChange={handleChange}
-                                            onBlur={handleBlur}
-                                            error={Boolean(touched.amount && errors.amount)}
-                                            helperText={touched.amount && errors.amount}
-                                        />
-                                        <TextField
-                                            name="rate"
-                                            type="number"
-                                            label={'Enter expected rate *'}
-                                            InputProps={{endAdornment: <InputAdornment position="end">%</InputAdornment>}}
-                                            value={values.rate}
-                                            onChange={handleChange}
-                                            onBlur={handleBlur}
-                                            error={Boolean(touched.rate && errors.rate)}
-                                            helperText={touched.rate && errors.rate}
-                                        />
-                                        <TextField
-                                            name="loanStartDate"
-                                            type="date"
-                                            label="Start loan date"
-                                            InputLabelProps={{shrink: true}}
-                                            onChange={handleChange}
-                                            onBlur={handleBlur}
-                                        />
-                                        <Typography>Duration of a loan in months</Typography>
-                                        <Slider
-                                            name="loanDuration"
-                                            valueLabelDisplay="auto"
-                                            step={1}
-                                            min={1}
-                                            max={24}
-                                            marks={marks}
-                                            value={values.loanDuration}
-                                            onChange={(e, value) => setFieldValue('loanDuration', value)}
-                                        />
+                            {({handleSubmit, values, handleChange, handleBlur, errors, touched, setFieldValue, isValid}) => {
+                                const loanAmount = values.amount! + (values.rate! / 100) * values.loanDuration;
+                                return (
+                                    <Form onSubmit={handleSubmit}>
+                                        <FormWrapper>
+                                            <Typography>
+                                                Amount to pay: {!Number.isNaN(Number(loanAmount)) ? ` ${loanAmount} zł` : ' 0 zł'}{' '}
+                                            </Typography>
+                                            <TextField
+                                                autoFocus
+                                                name="amount"
+                                                type="number"
+                                                label={'Enter amount *'}
+                                                value={values.amount}
+                                                onChange={handleChange}
+                                                onBlur={handleBlur}
+                                                error={Boolean(touched.amount && errors.amount)}
+                                                helperText={touched.amount && errors.amount}
+                                            />
+                                            <TextField
+                                                name="rate"
+                                                type="number"
+                                                label={'Enter expected rate *'}
+                                                InputProps={{endAdornment: <InputAdornment position="end">%</InputAdornment>}}
+                                                value={values.rate}
+                                                onChange={handleChange}
+                                                onBlur={handleBlur}
+                                                error={Boolean(touched.rate && errors.rate)}
+                                                helperText={touched.rate && errors.rate}
+                                            />
+                                            <TextField
+                                                name="loanStartDate"
+                                                type="date"
+                                                label="Start loan date"
+                                                InputLabelProps={{shrink: true}}
+                                                onChange={handleChange}
+                                                onBlur={handleBlur}
+                                            />
+                                            <Typography>Duration of a loan in months</Typography>
+                                            <Slider
+                                                name="loanDuration"
+                                                valueLabelDisplay="auto"
+                                                step={1}
+                                                min={1}
+                                                max={24}
+                                                marks={marks}
+                                                value={values.loanDuration}
+                                                onChange={(e, value) => setFieldValue('loanDuration', value)}
+                                            />
 
-                                        <Button type="submit" disabled={!isValid}>
-                                            Create auction
-                                        </Button>
-                                    </FormWrapper>
-                                </Form>
-                            )}
+                                            <Button type="submit" disabled={!isValid}>
+                                                Create auction
+                                            </Button>
+                                        </FormWrapper>
+                                    </Form>
+                                );
+                            }}
                         </Formik>
                     </CreateAuctionCardWrapper>
                 </AccordionWrapper>
