74951f48e692c73f7c6c0c6fe52cbd2df996728c
Michal Borowski <unpolacoo@gmail.com>
2020-11-18 19:27:26 +0100

    Add mock to child components in router tests

--- test/integration/pages/BorrowerPage/Commitments/BorrowerCommitments.test.tsx
+++ test/integration/pages/BorrowerPage/Commitments/BorrowerCommitments.test.tsx
@@ -1,26 +1,36 @@
-import {screen} from '@testing-library/react';
-import {Portfolio} from '../../../../../src/components/pages/LenderPage/Portfolio/Portfolio';
 import React from 'react';
+import {screen} from '@testing-library/react';
+import {mocked} from 'ts-jest/utils';
 import userEvent from '@testing-library/user-event';
 import {renderWithRouter} from '../../../../helpers/renderWithRouter';
+import {CommitmentsAuctions} from '../../../../../src/components/pages/BorrowerPage/Commitments/CommitmentsAuctions';
+import {CommitmentsLoans} from '../../../../../src/components/pages/BorrowerPage/Commitments/CommitmentsLoans';
+import {Commitments} from '../../../../../src/components/pages/BorrowerPage/Commitments/Commitments';
 
-describe('Portfolio', () => {
+jest.mock('../../../../../src/components/pages/BorrowerPage/Commitments/CommitmentsLoans');
+jest.mock('../../../../../src/components/pages/BorrowerPage/Commitments/CommitmentsAuctions');
+
+describe('Commitments', () => {
+    beforeEach(() => {
+        mocked(CommitmentsAuctions).mockReturnValue(<h2>CommitmentsAuctions</h2>);
+        mocked(CommitmentsLoans).mockReturnValue(<h2>CommitmentsLoans</h2>);
+    });
     it('renders tabs', async () => {
-        const {getByRole} = renderWithRouter(<Portfolio />);
-        expect(screen.getByRole('tab', {name: /My offers/i})).toBeInTheDocument();
-        expect(screen.getByRole('tab', {name: /My investments/i})).toBeInTheDocument();
+        const {getByRole} = renderWithRouter(<Commitments />);
+        expect(screen.getByRole('tab', {name: /My auctions/i})).toBeInTheDocument();
+        expect(screen.getByRole('tab', {name: /My loans/i})).toBeInTheDocument();
     });
 
-    it('sets /lender/portfolio/user-offers url on click on Invest tab', () => {
-        const {history} = renderWithRouter(<Portfolio />);
-        userEvent.click(screen.getByRole('tab', {name: /My offers/i}));
+    it('sets /borrower/commitments/auctions url on click on My auctions tab', () => {
+        const {history} = renderWithRouter(<Commitments />);
+        userEvent.click(screen.getByRole('tab', {name: /My auctions/i}));
         expect(history.entries).toHaveLength(2);
-        expect(history.location.pathname).toEqual('/lender/portfolio/user-offers');
+        expect(history.location.pathname).toEqual('/borrower/commitments/auctions');
     });
-    it('sets /lender/portfolio/user-investments url on click on Portfolio button', () => {
-        const {history} = renderWithRouter(<Portfolio />);
-        userEvent.click(screen.getByRole('tab', {name: /My investments/i}));
+    it('sets /lender/portfolio/user-investments url on click on My loans tab', () => {
+        const {history} = renderWithRouter(<Commitments />);
+        userEvent.click(screen.getByRole('tab', {name: /My loans/i}));
         expect(history.entries).toHaveLength(2);
-        expect(history.location.pathname).toEqual('/lender/portfolio/user-investments');
+        expect(history.location.pathname).toEqual('/borrower/commitments/loans');
     });
 });
