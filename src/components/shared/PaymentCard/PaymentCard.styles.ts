f2d37068e0a079afcab6b14a971cc75c22973582
Michal Borowski <unpolacoo@gmail.com>
2020-11-22 23:35:01 +0100

    Create PaymentCard and PaymentForms components

---
+++ src/components/shared/PaymentCard/PaymentCard.styles.ts
@@ -0,0 +1,15 @@
+import styled from '@emotion/styled';
+import {Card} from '@material-ui/core/';
+
+export const StyledCard = styled(Card)`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 500px;
+    padding: 8px;
+`;
+export const TextBold = styled.p`
+    font-weight: 500;
+    font-size: 17px;
+    margin: 20px;
+`;
---
+++ src/components/shared/PaymentCard/PaymentCard.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import {CircularProgress, Snackbar} from '@material-ui/core';
+import {PaymentFormOnUserAccount} from '../PaymentForm/PaymentFormOnUserAccount';
+import {PaymentFormOnPlatformAccount} from '../PaymentForm/PaymentFormOnPlatformAccount';
+import {StyledCard, TextBold} from './PaymentCard.styles';
+import {usePaymentOnPlatformAccount} from '../../../hooks/usePaymentOnPlatformAccount';
+import {usePaymentOnUserAccount} from '../../../hooks/usePaymentOnUserAccount';
+import {Alert} from '../Alert/Alert';
+
+export const PaymentCard: React.FC<any> = ({accountBalance, currentPage}) => {
+    const {
+        isFetchingPaymentOnPlatform,
+        isErrorPaymentOnPlatform,
+        fetchPaymentOnPlatformAccount,
+        responsePaymentOnPlatform,
+    } = usePaymentOnPlatformAccount();
+    const {
+        isFetchingPaymentOnUserAccount,
+        isErrorPaymentOnUserAccount,
+        fetchPaymentOnUserAccount,
+        responsePaymentOnUserAccount,
+    } = usePaymentOnUserAccount();
+
+    let openSuccess = false;
+    let openError = false;
+
+    if (isFetchingPaymentOnPlatform || isFetchingPaymentOnUserAccount) {
+        return <CircularProgress />;
+    }
+    if (isErrorPaymentOnPlatform || isErrorPaymentOnUserAccount) {
+        openError = true;
+        openSuccess = false;
+    }
+    if (responsePaymentOnUserAccount || responsePaymentOnPlatform) {
+        openError = false;
+        openSuccess = true;
+    }
+
+    return (
+        <StyledCard>
+            <TextBold>Your platform account balance: {accountBalance} zł</TextBold>
+            <PaymentFormOnPlatformAccount currentPage={currentPage} fetchPaymentOnPlatformAccount={fetchPaymentOnPlatformAccount} />
+            <PaymentFormOnUserAccount currentPage={currentPage} fetchPaymentOnUserAccount={fetchPaymentOnUserAccount} />
+            <Snackbar open={openSuccess} autoHideDuration={4000}>
+                <Alert severity="success">'Your payment was successfull!'</Alert>
+            </Snackbar>
+            <Snackbar open={openError} autoHideDuration={4000}>
+                <Alert severity="error">'Something went wrong with your payment. Please try again.'</Alert>
+            </Snackbar>
+        </StyledCard>
+    );
+};
