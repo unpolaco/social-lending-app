5bd97309eee80e52e197acadbcbce3a05010f6c4
Michal Borowski <unpolacoo@gmail.com>
2020-11-23 10:24:11 +0100

    Add CollapseBoxCreateOffer test

--- test/integration/shared/TableCollapseBox/CollapseBoxCreateOffer.test.tsx
+++ test/integration/shared/TableCollapseBox/CollapseBoxCreateOffer.test.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {render, screen} from '@testing-library/react';
 import {CollapseBoxCreateOffer} from '../../../../src/components/shared/TableCollapseBox/CollapseBoxCreateOffer';
 import {OfferData} from '../../../../src/components/shared/Table/Table.types';
+import userEvent from '@testing-library/user-event';
 describe('Formik in CollapseBoxCreateOffer component', () => {
     const row: OfferData = {amount: 0, id: 0, rate: 0, status: ''};
     const handleSaveNewOffer: any = jest.fn();
@@ -11,4 +12,35 @@ describe('Formik in CollapseBoxCreateOffer component', () => {
         expect(screen.getByRole('spinbutton')).toBeInTheDocument();
         expect(screen.getByRole('button')).toBeInTheDocument();
     });
+
+    // it('show "Such a big rate is not allowed" error when "21" is typed', async () => {
+    //     const {getByRole, findByText} = render(<CollapseBoxCreateOffer row={row} handleSaveNewOffer={handleSaveNewOffer}/>);
+    //     userEvent.clear(screen.getByRole('spinbutton'));
+    //     userEvent.type(screen.getByRole('spinbutton'), '21');
+    //     userEvent.click(screen.getByRole('button', {name: /Create offer/i}))
+    //     expect(await screen.findByText('Such a big rate is not allowed')).toBeInTheDocument();
+    // });
+    it('show "Enter correct rate" error when "-1" is typed', async () => {
+        const {getByRole, findByText} = render(<CollapseBoxCreateOffer row={row} handleSaveNewOffer={handleSaveNewOffer} />);
+        userEvent.clear(screen.getByRole('spinbutton'));
+        userEvent.type(screen.getByRole('spinbutton'), '-1');
+        userEvent.click(screen.getByRole('button', {name: /Create offer/i}));
+        expect(await screen.findByText('Enter correct rate')).toBeInTheDocument();
+    });
+
+    it('show "Enter correct rate" error when "0" is typed', async () => {
+        const {getByRole, findByText} = render(<CollapseBoxCreateOffer row={row} handleSaveNewOffer={handleSaveNewOffer} />);
+        userEvent.clear(screen.getByRole('spinbutton'));
+        userEvent.type(screen.getByRole('spinbutton'), '0');
+        userEvent.click(screen.getByRole('button', {name: /Create offer/i}));
+        expect(await screen.findByText('Enter correct rate')).toBeInTheDocument();
+    });
+    // it('make button disabled when "0" is typed', async () => {
+    //     const {getByRole, findByText} = render(<CollapseBoxCreateOffer row={row} handleSaveNewOffer={handleSaveNewOffer}/>);
+
+    //     userEvent.clear(screen.getByRole('spinbutton'));
+    //     userEvent.type(screen.getByRole('spinbutton'), '0');
+    //     userEvent.click(screen.getByRole('button', {name: /Create offer/i}))
+    //     expect(screen.getByRole('button', {name: /Create offer/i})).toBeDisabled()
+    // });
 });
