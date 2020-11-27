3455248490139d80dae32fc38b4a447e9af947b7
Michal Borowski <unpolacoo@gmail.com>
2020-11-27 21:43:41 +0100

    Create payment tests

--- test/integration/shared/PaymentForm/PaymentFormOnUserAccount.test.tsx
+++ test/integration/shared/PaymentForm/PaymentFormOnUserAccount.test.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import {render, screen, waitFor} from '@testing-library/react';
-import {PaymentFormOnPlatformAccount} from '../../../../src/components/shared/PaymentForm/PaymentFormOnPlatformAccount';
+import {PaymentFormOnUserAccount} from '../../../../src/components/shared/PaymentForm/PaymentFormOnUserAccount';
 import userEvent from '@testing-library/user-event';
 describe('testing AuctionCreateForm component', () => {
     const fetchPaymentOnUserAccount: any = jest.fn();
 
     it('renders input', async () => {
         const {getByLabelText, getByTestId} = render(
-            <PaymentFormOnPlatformAccount fetchPaymentOnUserAccount={fetchPaymentOnUserAccount} currentPage="lender" />,
+            <PaymentFormOnUserAccount fetchPaymentOnUserAccount={fetchPaymentOnUserAccount} currentPage="lender" />,
         );
         expect(screen.getByLabelText('Enter amount *')).toBeInTheDocument();
         expect(screen.getByTestId('buttonPayment')).toBeInTheDocument();
@@ -15,21 +15,21 @@ describe('testing AuctionCreateForm component', () => {
 
     it('make button disabled when "-1" is typed in amount input', async () => {
         const {getByLabelText, getByTestId} = render(
-            <PaymentFormOnPlatformAccount fetchPaymentOnUserAccount={fetchPaymentOnUserAccount} currentPage="lender" />,
+            <PaymentFormOnUserAccount fetchPaymentOnUserAccount={fetchPaymentOnUserAccount} currentPage="lender" />,
         );
         await userEvent.type(screen.getByLabelText('Enter amount *'), '-1');
         await waitFor(() => expect(screen.getByTestId('buttonPayment')).toBeDisabled());
     });
     it('make button disabled when "0" is typed in amount input', async () => {
         const {getByLabelText, getByTestId} = render(
-            <PaymentFormOnPlatformAccount fetchPaymentOnUserAccount={fetchPaymentOnUserAccount} currentPage="lender" />,
+            <PaymentFormOnUserAccount fetchPaymentOnUserAccount={fetchPaymentOnUserAccount} currentPage="lender" />,
         );
         await userEvent.type(screen.getByLabelText('Enter amount *'), '0');
         await waitFor(() => expect(screen.getByTestId('buttonPayment')).toBeDisabled());
     });
     it('make button enabled when input is filled with valid number', async () => {
         const {getByTestId} = render(
-            <PaymentFormOnPlatformAccount fetchPaymentOnUserAccount={fetchPaymentOnUserAccount} currentPage="lender" />,
+            <PaymentFormOnUserAccount fetchPaymentOnUserAccount={fetchPaymentOnUserAccount} currentPage="lender" />,
         );
         userEvent.type(screen.getByLabelText('Enter amount *'), '500');
         await waitFor(() => expect(screen.getByTestId('buttonPayment')).toBeEnabled());
